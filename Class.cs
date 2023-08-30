8cdc763aefa55423cfbd23bc4663b51f453a5fba
YousefMujahid <70110976+YousefMujahid@users.noreply.github.com>
2023-08-30 18:09:32 +0300

    Nothing serious

--- Class.cs
+++ Class.cs
@@ -1,15 +1,15 @@
- public class Class
+public class Class
 {
     Class course;
     string name;
-
+    string name2 = "";
 
     public Class()
     {
 
     }
 
-    public void createClass( Course course, string name )
+    public void createClass(Course course, string name)
     {
         this.name = name;
         this.course = course;
