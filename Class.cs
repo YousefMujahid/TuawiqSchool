3c8615d9264cd0e0444cecf310e2fad314750a5d
Leenah M <almashari.lm@gmail.com>
2023-08-30 18:02:43 +0300

    Leenah addition

---
+++ Class.cs
@@ -0,0 +1,19 @@
+ public class Class
+{
+    Class course;
+    string name;
+
+
+    public Class()
+    {
+
+    }
+
+    public void createClass( Course course, string name )
+    {
+        this.name = name;
+        this.course = course;
+    }
+
+
+}
---
+++ Student.cs
@@ -0,0 +1,47 @@
+using System;
+public class Student
+{
+    String name;
+    int nID;
+    int ID = 0; //as count
+    int level;
+    String DoB;
+    String homeTown;
+    int grade; 
+    Course courses = new Course[5];
+    int numberOfCourses=0;
+
+    public Student ()
+    {
+    }
+
+    public void register(String n, int id, int l, String dob, String ht)
+    {
+        name = n;
+        ID = id;
+        level = l;
+        DoB = dob;
+        homeTown = ht;
+        ID = ++ID;
+    }
+
+    public addCourse(String name)
+    {
+        //Search ,, show list
+
+
+        //input course name 
+
+        // check if #c < 5 >> ok
+
+        // assign course 
+
+    }
+
+    
+
+
+}
+
+
+// Person : Student >> inherent from 
